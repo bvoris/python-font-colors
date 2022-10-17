3631edbe65c856280558f645b38ca71816396fd2
Brad Voris <40213182+bvoris@users.noreply.github.com>
2022-10-17 09:27:19 -0500

    Add files via upload

---
+++ Font Colors.py
@@ -0,0 +1,42 @@
+print('\033[2;31;43m CHEESY')
+
+# [Fount Style, Text Color, Background Color]
+print('\033[1;1;43m CHEESY')
+#Text Styles:
+#    Normal 0
+#    Bold 1
+#    Light 2
+#    Italicized 3
+#    Underlined 4
+#    Blink 5
+
+#Font Style Test
+print("Font Style Test")
+print('\033[0;30;47m TEST')
+print('\033[1;30;47m TEST')
+print('\033[2;30;47m TEST')
+print('\033[3;30;47m TEST')
+print('\033[4;30;47m TEST')
+print('\033[5;30;47m TEST')
+print()
+#Font Color Test
+print("Font Color Test")
+print('\033[0;30;47m TEST')
+print('\033[0;31;47m TEST')
+print('\033[0;32;47m TEST')
+print('\033[0;33;47m TEST')
+print('\033[0;34;47m TEST')
+print('\033[0;35;47m TEST')
+print('\033[0;36;47m TEST')
+print('\033[0;37;47m TEST')
+print()
+#Background Color Test
+print("Background Color Test")
+print('\033[0;30;40m TEST')
+print('\033[0;30;41m TEST')
+print('\033[0;30;42m TEST')
+print('\033[0;30;43m TEST')
+print('\033[0;30;44m TEST')
+print('\033[0;30;45m TEST')
+print('\033[0;30;46m TEST')
+print('\033[0;30;47m TEST')
